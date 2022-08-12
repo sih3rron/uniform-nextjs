12769bffabce9942af3e779229f61e4d1097012b
sih3rron <siherron+github@gmail.com>
2022-08-12 09:22:21 +0100

    Add a function to log out P13n metadata. Need to find source of data in order to augment

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import '../styles/globals.css'
 import { UniformContext } from "@uniformdev/context-react";
-import { createUniformContext } from '../functions/uniformContext';
-
+import { createUniformContext } from '../functions/uniformContext'
 const clientContext = createUniformContext();
 
-function MyApp({ Component, pageProps, serverUniformContext}) {
+const options = {
+  emitAll: true,
+  disableEventSignals: true,
+}
+
+const pzEmitter = PersonalizationEvent => {
+  console.groupCollapsed("%c Uniform Personalization Data", 'font-weight: bold; font-size: 12px; background: #32CD32; color: #000; padding: 7px; ')
+  console.log("Received a Personalization Event", PersonalizationEvent);
+  console.groupEnd()
+}
+
+
+function MyApp({ Component, pageProps, serverUniformContext}) {  
+  clientContext.events.on('personalizationResult', pzEmitter)
   return (
     <UniformContext context={serverUniformContext ?? clientContext} >
       <Component {...pageProps} />
